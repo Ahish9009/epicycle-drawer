10fc7ec9fa6076e8c489d511c1e7ed4d6baff1a0
Ahish9009 <ahishjayant9009@gmail.com>
2019-12-31 16:15:06 +0400

    Added circles to 3D plot

--- _helperFuncs/animation_funcs.py
+++ _helperFuncs/animation_funcs.py
@@ -8,7 +8,7 @@ def pos_at(time, abs_fft, phase_fft):
     loc_x = 0
     loc_y = 0
     w = 2*math.pi/N
-
+    
     for i in range(N):
 
         loc_x += abs_fft[i]*math.cos((i)*w*time + phase_fft[i])
